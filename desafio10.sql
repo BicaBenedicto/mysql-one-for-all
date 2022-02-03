f735579c99eb6dbd5657501ccebd305686decef9
Gabriel <gabrielpbenedicto@gmail.com>
2022-02-03 15:55:57 -0300

    feat: Query musicas mais ouvidas no plano gratuito e pessoal

--- desafio10.sql
+++ desafio10.sql
@@ -3,6 +3,9 @@ SELECT
     COUNT(hR.cancao_id) AS reproducoes
 FROM SpotifyClone.cancoes AS c
 JOIN SpotifyClone.historicoReproducoes AS hR
+JOIN SpotifyClone.usuarios AS u
 WHERE c.cancao_id = hR.cancao_id
+AND u.plano_id IN (1, 4)
+AND u.usuario_id = hR.usuario_id
 GROUP BY c.cancao
 ORDER BY c.cancao;
